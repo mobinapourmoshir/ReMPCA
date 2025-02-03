4b4af12f712d0c4c42a8beb8dced289e93da88e1
Mobina Pourmoshir <mobinapourmoshir@gmail.com>
2025-02-03 10:13:18 -0600

    Done

--- R/mhdClass.R
+++ R/mhdClass.R
@@ -1,44 +1,88 @@
-#' @title  Define a Set of Multivariate Hybrid Data objects
+#' @title  Define an object for the elements of the hybrid data.
 #'
 #' #' @description
-#' The `mhd` class represents a set of multivariate hybrid data.
-#' Functional data Objects are constructed using matrices, with columns representing grid points and rows indicating observations.
+#' The `hd` class denotes an element of hybrid data, which can be either functional data or regular data.
+#' Data that is functional Objects are constructed using matrices, with columns representing grid points and rows indicating observations.
+#' The Smoothing_parameter attributes are the distinguishing factor between the functional and regular data.
+#' The smoothness of the data will not be implemented if the value is zero, or user can choose a fix number for smoothing parameter.
+#' Alternatively, a vector of numerical values that will undergo generalized cross validation (GCV) to determine the most optimal one.
 #'
-#' @param argval A list of numeric vectors of argument values at which the `mfd` object is to be evaluated
 #'
-#' @param mvfd_obj List of matrices or arrays (Multivariate Grid Functional Data)
-#' @param centerfns logical. If TRUE  the input data undergoes centralization or demeaning prior to being processed by the function.
-#' @param num_pcs  The number of PCs. The default is one (The first principal component only). But the user is able to see higher PCs
-#' @description Check for validity of the data in the `mvgfd` object
+#' @param data A matrix which represents the data with rows indicating observations.
+#' @param argval A vector of grid points for functional data, with a length equal to the number of columns in the data.
+#'
+#' @param Smoothing_parameter A fix number representing the smoothing parameter,
+#' or a vector of numerical values that will undergo generalized cross validation (GCV) to determine the most optimal one.
+#' Set it to 0 for no smoothing.
+#' @param Sparsity_parameter A fix number representing the level of sparsity,
+#' or a vector of numerical values that will undergo cross validation (CV) to determine the most optimal one.
+#' For no sparsity, set it to 0 and tune it automatically by setting it to NULL.
+#'
+#' @description Check for validity of the data in the `hd` object
 #'
 #' @examples
 #'
 #' # Create some example matrices
-#' obj1 <- hdClass(mat, attr = NULL)
-#' obj2 <- hdClass(mat, attr = 0)
+#' mat1 <- matrix(c(1:30), nrow = 5)
+#' mat2 <- matrix(c(1:25), nrow = 5)
+#'
+#' obj1 <- hdClass(mat1,
+#'                 argval = NULL,
+#'                 Smoothing_parameter = 0,
+#'                 Sparsity_parameter = NULL)
+#'
+#' obj2 <- hdClass(mat2, attr = 0)
+#'
+#' hybrid.data <- list(obj1, obj2)
 #'
 #' # Print the object
 #' print(obj1)
+#'
+#'
 #' print(obj2)
 #'
 #' @export
 
-
 ####################### Define an S3 Class for Hybrid Data #######################
-hdClass <- function(data, attr = 0) {
+hdClass <- function(data,
+                    argval = NULL,
+                    Smoothing_parameter = 0,
+                    Sparsity_parameter = 0) {
+
   if (!is.matrix(data)) {
     stop("Input 'data' must be a matrix.")
   }
 
-  # Validate 'attr' to be a number, vector, 0, or NULL
-  if (!is.null(attr) && !is.numeric(attr) && !identical(attr, 0)) {
-    stop("Attribute must be a numeric value, numeric vector, 0, or NULL.")
+  # Validate 'Smoothing_parameter' to be a number, vector, 0, or NULL
+  if (!is.null(Smoothing_parameter) &&
+      !is.numeric(Smoothing_parameter) &&
+      !identical(Smoothing_parameter, 0)) {
+    stop("Smoothing_parameter must be a numeric value, numeric vector, 0, or NULL.")
   }
 
   # Assign the attribute to the matrix
-  if(is.null(attr)){
-    attr <- 2^seq(-30,5, length.out = 10)}
-  attr(data, "custom_attr") <- attr
+  if(is.null(Smoothing_parameter)){
+    Smoothing_parameter <- 2^seq(-30,5, length.out = 10)}
+  attr(data, "Smoothing_parameter") <- Smoothing_parameter
+
+
+  # Validate 'Sparsity_parameter' to be a number, vector, 0, or NULL
+  if (!is.null(Sparsity_parameter) &&
+      !is.numeric(Sparsity_parameter) &&
+      !identical(Sparsity_parameter, 0)) {
+    stop("Sparsity_parameter must be a numeric value, numeric vector, 0, or NULL.")
+  }
+
+  if(any(Sparsity_parameter > ncol(data))){
+    warning("An integer between 0 and ncol(data) must be used to represent the level of sparsity. Setting the 'Sparsity_parameter' to NULL!")
+    Sparsity_parameter <- NULL
+  }
+
+  # Assign the attribute to the matrix
+  if(is.null(Sparsity_parameter)){
+    Sparsity_parameter <- seq(from = 0, to = ncol(data)-1, by = 1)}
+  attr(data, "Sparsity_parameter") <- Sparsity_parameter
+
 
   # Set the class of the object
   class(data) <- "hd"
--- R/mhdMethods.R
+++ R/mhdMethods.R
@@ -1,8 +1,30 @@
 ################# Method for `hd` class #################
-# Custom print method for the S3 object
-print.hdClass <- function(x, ...) {
-  cat("My Hybrid data Object:\n")
-  print(unclass(x))  # Print the matrix without the class
-  cat("\nCustom Attribute:\n")
-  print(attr(x, "custom_attr"))
+print.hd <- function(x, ...) {
+  cat("Hybrid Data (hdClass) Object\n")
+  cat("-----------------------------------\n")
+  cat("Dimensions: ", dim(x)[1], "x", dim(x)[2], "\n")
+
+  # Print the Smoothing parameter
+  smoothing_param <- attr(x, "Smoothing_parameter")
+  cat("Smoothing Parameter: ")
+  if (length(smoothing_param) > 5) {
+    cat(head(smoothing_param, 3), "...", tail(smoothing_param, 2), "\n")
+  } else {
+    cat(smoothing_param, "\n")
+  }
+
+  # Print the Sparsity parameter
+  sparsity_param <- attr(x, "Sparsity_parameter")
+  cat("Sparsity Parameter: ")
+  if (length(sparsity_param) > 5) {
+    cat(head(sparsity_param, 3), "...", tail(sparsity_param, 2), "\n")
+  } else {
+    cat(sparsity_param, "\n")
+  }
+
+  cat("-----------------------------------\n")
+  cat("First few rows of the data:\n")
+  print(head(x))
+
+  invisible(x)  # Return the object invisibly
 }
