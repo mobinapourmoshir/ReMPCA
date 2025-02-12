431d2705fd9029e6e1865b6d7f7491b06ae3162d
Mobina Pourmoshir <mobinapourmoshir@gmail.com>
2025-02-12 15:02:55 -0600

    Done

--- R/mhdMethods.R
+++ R/mhdMethods.R
@@ -5,12 +5,48 @@ print.hd <- function(x, ...) {
 
   # Get attributes
   smoothing_param <- attr(x, "Smoothing_parameter")
-  two_way_smoothness <- attr(x, "two_way_smoothness")
   Sparsity_parameter_col <- attr(x, "Sparsity_parameter_col")
   Sparsity_parameter_row <- attr(x, "Sparsity_parameter_row")
 
+
+  # Print Smoothing Parameter
+  cat("Smoothing Parameter: ")
+  if (!is.null(smoothing_param)) {
+    if (length(smoothing_param) > 5) {
+      cat(head(smoothing_param, 3), "...", tail(smoothing_param, 2), "\n")
+    } else {
+      cat(smoothing_param, "\n")
+    }
+  } else {
+    cat("NULL\n")
+  }
+
+  # Print Sparsity Parameters
+  cat("Columns Sparsity Parameter: ")
+  if (!is.null(Sparsity_parameter_col)) {
+    if (length(Sparsity_parameter_col) > 5) {
+      cat(head(Sparsity_parameter_col, 3), "...", tail(Sparsity_parameter_col, 2), "\n")
+    } else {
+      cat(Sparsity_parameter_col, "\n")
+    }
+  } else {
+    cat("NULL\n")
+  }
+
+  cat("Rows Sparsity Parameter: ")
+  if (!is.null(Sparsity_parameter_row)) {
+    if (length(Sparsity_parameter_row) > 5) {
+      cat(head(Sparsity_parameter_row, 3), "...", tail(Sparsity_parameter_row, 2), "\n")
+    } else {
+      cat(Sparsity_parameter_row, "\n")
+    }
+  } else {
+    cat("NULL\n")
+  }
+
+
   # Check if the data is functional or regular
-  is_functional <- !identical(smoothing_param, 0) || !identical(two_way_smoothness, 0)
+  is_functional <- !identical(smoothing_param, 0)
 
   if (is_functional) {
     cat("Data Type: Functional Data\n")
@@ -41,53 +77,6 @@ print.hd <- function(x, ...) {
     }
   } else {
     cat("Data Type: Regular Data\n")
-  }
-
-  # Print Smoothing Parameter
-  cat("Smoothing Parameter: ")
-  if (!is.null(smoothing_param)) {
-    if (length(smoothing_param) > 5) {
-      cat(head(smoothing_param, 3), "...", tail(smoothing_param, 2), "\n")
-    } else {
-      cat(smoothing_param, "\n")
-    }
-  } else {
-    cat("NULL\n")
-  }
-
-  # Print Two-Way Smoothing Parameter
-  cat("Two-Way Smoothing Parameter: ")
-  if (!is.null(two_way_smoothness)) {
-    if (length(two_way_smoothness) > 5) {
-      cat(head(two_way_smoothness, 3), "...", tail(two_way_smoothness, 2), "\n")
-    } else {
-      cat(two_way_smoothness, "\n")
-    }
-  } else {
-    cat("NULL\n")
-  }
-
-  # Print Sparsity Parameters
-  cat("Columns Sparsity Parameter: ")
-  if (!is.null(Sparsity_parameter_col)) {
-    if (length(Sparsity_parameter_col) > 5) {
-      cat(head(Sparsity_parameter_col, 3), "...", tail(Sparsity_parameter_col, 2), "\n")
-    } else {
-      cat(Sparsity_parameter_col, "\n")
-    }
-  } else {
-    cat("NULL\n")
-  }
-
-  cat("Rows Sparsity Parameter: ")
-  if (!is.null(Sparsity_parameter_row)) {
-    if (length(Sparsity_parameter_row) > 5) {
-      cat(head(Sparsity_parameter_row, 3), "...", tail(Sparsity_parameter_row, 2), "\n")
-    } else {
-      cat(Sparsity_parameter_row, "\n")
-    }
-  } else {
-    cat("NULL\n")
   }
 
   cat("-----------------------------------\n")
