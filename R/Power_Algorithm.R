1d971a662822a1ba7a665d7c567846f007043614
Mobina Pourmoshir <mobinapourmoshir@gmail.com>
2025-05-06 18:22:20 -0500

    *Adding Conditional

--- R/Power_Algorithm.R
+++ R/Power_Algorithm.R
@@ -1,4 +1,4 @@
-############################### Sparse penalty for coefficients ###############################
+################ Sparse penalty for coefficients ################
 # Lemma 2 (Sparse PCA via regularized low rank matrix approximation by Huang)
 # y is either coefficients (u's) or PCs (v's)
 sparse_pen_fun <- function(y,
@@ -31,63 +31,78 @@ sparse_pen_fun <- function(y,
   }
 }
 
-############################### Calculating the norm of a vector ###############################
+##################### Calculating the norm of a vector #####################
 norm_vec <- function(x) sqrt(sum(x^2))
 
 ############################### Power Algorithm ###############################
-power_algo = function(data,
-                      n_var,
-                      ncol,
-                      sparse_tuning_result_u, # A fixed number
-                      sparse_tuning_result_v, # A vector (length = p)
-                      S_alpha_v, # A list (lenght = p)
-                      S_alpha_u, # A matrix
-                      sparse_tuning_type){
-
+power_algo <- function(data,
+                       n_var,
+                       ncol,
+                       sparse_tuning_result_u,
+                       sparse_tuning_result_v,
+                       S_alpha_v,
+                       S_alpha_u,
+                       sparse_tuning_type,
+                       conditional = FALSE,
+                       alpha_Omega_v,
+                       alpha_Omega_u) {
   rownames(data) <- NULL; colnames(data) <- NULL
   data <- as.matrix(data)
 
-  # Splitting data
   start_col <- 1; splitted_data <- list()
   for (i in 1:n_var) {
-    num_cols <- ncol[[i]]  # Get number of columns for the ith matrix
-    splitted_data[[i]] <- data[, start_col:(start_col + num_cols - 1)]  # Extract matrix
+    num_cols <- ncol[[i]]
+    splitted_data[[i]] <- data[, start_col:(start_col + num_cols - 1)]
     start_col <- start_col + num_cols
   }
 
-  v_old <- svd(as.matrix(data))$v[, 1]
+  v_old <- svd(data)$v[, 1]
   errors <- Inf; thresh <- 1e-10
 
-  # Power Algorithm
   while (errors > thresh) {
-    u_new <- S_alpha_u %*% sparse_pen_fun(y = data%*%as.matrix(v_old),
-                                          tuning_parameter = sparse_tuning_result_u,
-                                          type = sparse_tuning_type) # u = h_{gamma} Xv
-    if(all(u_new == 0)){ # To avoid 0 in the denominator
-      u_new <- S_alpha_u %*% data%*%v_old
-      }
+    # Compute u
+    Xv <- data %*% v_old
+    u_raw <- sparse_pen_fun(y = Xv,
+                            tuning_parameter = sparse_tuning_result_u,
+                            type = sparse_tuning_type)
+    if (all(u_raw == 0)) u_raw <- Xv
+
+    if (conditional) {
+      v_old <- as.vector(v_old)
+      alpha_Omega_v <- as.matrix(alpha_Omega_v)
+      storage.mode(alpha_Omega_v) <- "numeric"
+      denom_u <- as.numeric(t(v_old) %*%
+                              (diag(nrow(alpha_Omega_v)) + alpha_Omega_v) %*%
+                              v_old)
+      u_new <- S_alpha_u %*% u_raw / denom_u
+    } else {
+      u_new <- S_alpha_u %*% u_raw
+    }
+
+    # Compute v
     v_new <- c()
-    for(i in 1:n_var){
+    for (i in 1:n_var) {
       sparse_param <- as.integer(sparse_tuning_result_v[i])
-      v <- S_alpha_v[[i]] %*% sparse_pen_fun(y = t(splitted_data[[i]]) %*% as.matrix(u_new),
-                                             tuning_parameter = sparse_param,
-                                             type = sparse_tuning_type) # v = S_{alpha} h_{gamma} t(X)u
+      Xu_i <- t(splitted_data[[i]]) %*% u_new
+      v_part_raw <- sparse_pen_fun(y = Xu_i,
+                                   tuning_parameter = sparse_param,
+                                   type = sparse_tuning_type)
+      if (all(v_part_raw == 0)) v_part_raw <- Xu_i
 
-      if(all(v == 0)){ # To avoid 0 in the denominator
-        v <- S_alpha_v[[i]] %*% t(splitted_data[[i]]) %*% as.matrix(u_new)}
-      v_new <- c(v_new, v)
+      if (conditional) {
+        norm_u2 <- as.numeric(norm_vec(u_new)^2)
+        R_u <- as.numeric(t(u_new) %*% alpha_Omega_u %*% u_new / norm_u2)
+        denom_v <- 1 + R_u
+        v_part <- S_alpha_v[[i]] %*% as.vector(v_part_raw) / denom_v
+      } else {
+        v_part <- S_alpha_v[[i]] %*% as.vector(v_part_raw)
       }
-
-    # Adjust the sign of v based on the direction of maximum variance in the original data
-    #max_var_index = which.max(apply(data, 2, var))
-    #v_new = v_new * sign(v_new[max_var_index])
-    v_new = v_new / norm_vec(v_new) # v/||v||
-
-    # Convergence condition
-    errors = sum((v_new - v_old)^2)
+      v_new <- c(v_new, v_part)
+    }
+    v_new <- v_new / norm_vec(v_new)
+    errors <- sum((v_new - v_old)^2)
     v_old <- v_new
   }
-  u_new = u_new/norm_vec(u_new) # u/||u||
-  return(list(v_new,u_new))
+  if (!conditional) u_new <- u_new / norm_vec(u_new)
+  return(list(v_new, u_new))
 }
-
