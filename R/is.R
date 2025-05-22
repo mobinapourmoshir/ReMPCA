ad4a4d575a2d14a9a400755458fd391f34daa033
Mobina Pourmoshir <mobinapourmoshir@gmail.com>
2025-05-22 12:32:59 -0500

    * is() for Classes

---
+++ R/is.R
@@ -0,0 +1,30 @@
+#' Check if an object is of class 'fdClass'
+#'
+#' @param x An object to test.
+#' @return Logical; TRUE if the object inherits from class 'fdClass', FALSE otherwise.
+#'
+#' @export
+is.fdClass <- function(x) {
+  inherits(x, "fdClass")
+}
+
+#' Check if an object is of class 'rdClass'
+#'
+#' @param x An object to test.
+#' @return Logical; TRUE if the object inherits from class 'rdClass', FALSE otherwise.
+#'
+#' @export
+is.rdClass <- function(x) {
+  inherits(x, "rdClass")
+}
+
+#' Check if an object is of class 'hdClass'
+#'
+#' @param x An object to test.
+#' @return Logical; TRUE if the object inherits from class 'hdClass', FALSE otherwise.
+#'
+#' @export
+is.hdClass <- function(x) {
+  inherits(x, "hdClass")
+}
+
