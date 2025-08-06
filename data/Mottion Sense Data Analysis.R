c431d46d1b0487d02a3642d036419448df47909f
Mobina Pourmoshir <mobinapourmoshir@gmail.com>
2025-08-06 12:54:56 -0500

    * Motion sense data

--- data/Mottion Sense Data Analysis.R
+++ data/Mottion Sense Data Analysis.R
@@ -1,3 +1,9 @@
+library(ReMPCA)
+
+load("~/ReMPCA/data/motion_sense_data.rda")
+
+normfunc <- function(x) sqrt(sum(x^2))
+
 ######## Motion Sense Data #########
 X1obj <- fdClass(t(as.matrix(motion_sense_data$user_acceleration)),
                  Smoothing_parameter = 0,
@@ -33,7 +39,7 @@ matplot(
 
 # SVD #
 Motion_Sense_SVD <- ReMPCA(hd = Xobj,
-                           centerhds = FALSE,
+                           centerhds = TRUE,
                            num_pcs = 3,
                            nfolds_u = 5,
                            nfolds_v = NULL,
@@ -41,10 +47,10 @@ Motion_Sense_SVD <- ReMPCA(hd = Xobj,
                            maxit = 100,
                            tuning_iter = 1,
                            parallel = FALSE,
-                           weights = 0,
+                           weights = c(w1,w2),
                            smoothness_type = "Second_order",
                            sparse_tuning_type = "soft",
-                           tuning_order = "Sparsity",
+                           tuning_order = "Smoothness",
                            cv.pick = "min",
                            sparse_tuning_u = NULL,
                            sparse_tuning_v = NULL,
@@ -69,7 +75,7 @@ matplot(Motion_SVD_v32, type = 'l', main = 'v32')
 
 ##Smoothness on v #
 Motion_Sense_SmV <- ReMPCA(hd = Xobj,
-                           centerhds = FALSE,
+                           centerhds = TRUE,
                            num_pcs = 3,
                            nfolds_u = 5,
                            nfolds_v = NULL,
@@ -77,7 +83,7 @@ Motion_Sense_SmV <- ReMPCA(hd = Xobj,
                            maxit = 100,
                            tuning_iter = 1,
                            parallel = FALSE,
-                           weights = 0,
+                           weights = c(w1,w2),
                            smoothness_type = "Second_order",
                            sparse_tuning_type = "soft",
                            tuning_order = "Smoothness",
@@ -96,20 +102,27 @@ Motion_SmV_v32 <- Motion_Sense_SmV$PCFunctions[[3]][[2]]
 
 
 par(mfrow = c(2,3))
-matplot(Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
-points(Motion_SmV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
-points(Motion_SmV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
-points(Motion_SmV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
-points(Motion_SmV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
-points(Motion_SmV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
-points(Motion_SmV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SmV_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
+matplot(-Motion_SmV_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
+matplot(-Motion_SmV_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
+matplot(-Motion_SmV_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
+matplot(-Motion_SmV_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
+matplot(-Motion_SmV_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
 
 
+par(mfrow = c(2,3))
+matplot(-Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
+points(-Motion_SmV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
+points(-Motion_SmV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
+points(-Motion_SmV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
+points(-Motion_SmV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
+points(-Motion_SmV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
+points(-Motion_SmV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
 
 
 # Sparsity and Smoothness on v #
@@ -126,7 +139,7 @@ Xobj <- hdClass(list(X1obj,X2obj), Smoothing_parameter = 0,
                 Sparsity_parameter = 0)
 
 Motion_Sense_SSV <- ReMPCA(hd = Xobj,
-                           centerhds = FALSE,
+                           centerhds = TRUE,
                            num_pcs = 3,
                            nfolds_u = 5,
                            nfolds_v = NULL,
@@ -134,11 +147,11 @@ Motion_Sense_SSV <- ReMPCA(hd = Xobj,
                            maxit = 100,
                            tuning_iter = 1,
                            parallel = FALSE,
-                           weights = 0,
+                           weights = c(w1,w2),
                            smoothness_type = "Second_order",
                            sparse_tuning_type = "soft",
                            tuning_order = "Smoothness",
-                           cv.pick = "1se",
+                           cv.pick = "min",
                            sparse_tuning_u = NULL,
                            sparse_tuning_v = NULL,
                            smooth_tuning_u = NULL,
@@ -153,24 +166,25 @@ Motion_SSV_v32 <- Motion_Sense_SSV$PCFunctions[[3]][[2]]
 
 matplot(-Motion_SSV_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
 matplot(-Motion_SSV_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
-matplot(Motion_SSV_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
+matplot(-Motion_SSV_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
 matplot(-Motion_SSV_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
 matplot(-Motion_SSV_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
-matplot(Motion_SSV_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
+matplot(-Motion_SSV_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
 
 par(mfrow = c(2,3))
-matplot(Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
-points(Motion_SSV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
-points(Motion_SSV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
-points(Motion_SSV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
-points(Motion_SSV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
-points(Motion_SSV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
-points(Motion_SSV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
+points(-Motion_SSV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
+points(-Motion_SSV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
+points(-Motion_SSV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
+points(-Motion_SSV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
+points(-Motion_SSV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
+points(-Motion_SSV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
+
 
 
 
@@ -189,7 +203,7 @@ Xobj <- hdClass(list(X1obj,X2obj),
                 Sparsity_parameter = c(0,10,20,30,40,50,60,70,80,85))
 
 Motion_Sense_SS_UV <- ReMPCA(hd = Xobj,
-                             centerhds = FALSE,
+                             centerhds = TRUE,
                              num_pcs = 3,
                              nfolds_u = 5,
                              nfolds_v = NULL,
@@ -197,15 +211,15 @@ Motion_Sense_SS_UV <- ReMPCA(hd = Xobj,
                              maxit = 100,
                              tuning_iter = 1,
                              parallel = FALSE,
-                             weights = 0,
+                             weights = c(w1,w2),
                              smoothness_type = "Second_order",
                              sparse_tuning_type = "soft",
                              tuning_order = "Smoothness",
-                             cv.pick = "1se",
+                             cv.pick = "min",
                              sparse_tuning_u = NULL,
                              sparse_tuning_v = NULL,
                              smooth_tuning_u = NULL,
-                             smooth_tuning_v = list(4.485274e-05,3.027727e-06))
+                             smooth_tuning_v = list(4.485274e-05, 4.485274e-05))
 
 
 
@@ -216,59 +230,20 @@ Motion_SS_UV_v22 <- Motion_Sense_SS_UV$PCFunctions[[2]][[2]]
 Motion_SS_UV_v31 <- Motion_Sense_SS_UV$PCFunctions[[3]][[1]]
 Motion_SS_UV_v32 <- Motion_Sense_SS_UV$PCFunctions[[3]][[2]]
 
-Motion_Sense_SS_UV0 <- ReMPCA(hd = Xobj,
-                              centerhds = FALSE,
-                              num_pcs = 3,
-                              nfolds_u = 5,
-                              nfolds_v = NULL,
-                              thresh = 1e-10,
-                              maxit = 100,
-                              tuning_iter = 1,
-                              parallel = FALSE,
-                              weights = 0,
-                              smoothness_type = "Second_order",
-                              sparse_tuning_type = "soft",
-                              tuning_order = "Smoothness",
-                              cv.pick = "1se",
-                              sparse_tuning_u = NULL,
-                              sparse_tuning_v = list(0,0),
-                              smooth_tuning_u = NULL,
-                              smooth_tuning_v = list(0,0))
-
-Motion_SS_UV_v110 <- Motion_Sense_SS_UV0$PCFunctions[[1]][[1]]
-Motion_SS_UV_v120 <- Motion_Sense_SS_UV0$PCFunctions[[1]][[2]]
-Motion_SS_UV_v210 <- Motion_Sense_SS_UV0$PCFunctions[[2]][[1]]
-Motion_SS_UV_v220 <- Motion_Sense_SS_UV0$PCFunctions[[2]][[2]]
-Motion_SS_UV_v310 <- Motion_Sense_SS_UV0$PCFunctions[[3]][[1]]
-Motion_SS_UV_v320 <- Motion_Sense_SS_UV0$PCFunctions[[3]][[2]]
 
 par(mfrow = c(2,3))
-matplot(Motion_SS_UV_v110, type = 'l', , col = 'gray', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
-points(Motion_SS_UV_v11, type = 'l', col = 'black', lwd=2,xlab='Time')
-matplot(Motion_SS_UV_v210, type = 'l', , col = 'gray', main = 'v21',xlab='Time',  lwd=2)
-points(Motion_SS_UV_v21, type = 'l', col = 'black', lwd=2,xlab='Time')
-matplot(Motion_SS_UV_v310, type = 'l', , col = 'gray', main = 'v31',xlab='Time',  lwd=2)
-points(Motion_SS_UV_v31, type = 'l', col = 'black', lwd=2,xlab='Time')
-matplot(Motion_SS_UV_v120, type = 'l', , col = 'gray', main = 'v12',xlab='Time', ylab='Pitch attitude', lwd=2)
-points(Motion_SS_UV_v12, type = 'l', col = 'black', lwd=2,xlab='Time')
-matplot(Motion_SS_UV_v220, type = 'l', , col = 'gray', main = 'v22',xlab='Time',  lwd=2)
-points(Motion_SS_UV_v22, type = 'l', col = 'black', lwd=2,xlab='Time')
-
-
-
-par(mfrow = c(2,3))
-matplot(Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
-points(Motion_SS_UV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
-points(Motion_SS_UV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
-points(Motion_SS_UV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
-points(Motion_SS_UV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
-points(Motion_SS_UV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
-matplot(Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
-points(Motion_SS_UV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v11, type = 'l', main = 'v11',xlab='Time', ylab='User acceleration', lwd=2)
+points(-Motion_SS_UV_v11, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v21, type = 'l', main = 'v21', lwd=2,xlab='Time')
+points(-Motion_SS_UV_v21, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v31, type = 'l', main = 'v31', lwd=2,xlab='Time')
+points(-Motion_SS_UV_v31, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v12, type = 'l', main = 'v12', lwd=2,xlab='Time', ylab='Pitch attitude')
+points(-Motion_SS_UV_v12, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v22, type = 'l', main = 'v22', lwd=2,xlab='Time')
+points(-Motion_SS_UV_v22, type = 'l', col = 'red', lwd=2,xlab='Time')
+matplot(-Motion_SVD_v32, type = 'l', main = 'v32', lwd=2,xlab='Time')
+points(-Motion_SS_UV_v32, type = 'l', col = 'red', lwd=2,xlab='Time')
 
 
 plot(Motion_Sense_SVD$PCScores[,1], main = 'PC scores 1')
